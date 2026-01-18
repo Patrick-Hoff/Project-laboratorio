8961580e4b55d95fb392a37834df34ad84875c14
Patrick <patrickhoffmanncampos@gmail.com>
2026-01-17 22:53:09 -0300

    feat(agendamento): permitir busca por id opcional

--- api/controllers/agendamento.js
+++ api/controllers/agendamento.js
@@ -1,43 +1,59 @@
 import { db } from '../db.js'
 
 export const getAgendamentos = (req, res) => {
-    try {
+    const { id } = req.query;
 
-        const q = `
+    let q = `
         SELECT
-    id,
-    nome,
-    sexo,
-    cpf,
-    rg,
-    DATE_FORMAT(nascimento, '%d/%m/%Y') AS nascimento,
-    telefone,
-    email,
-    rua,
-    numero,
-    bairro,
-    cidade,
-    estado,
-    cep,
-    DATE_FORMAT(data_consulta, '%d-%m-%Y') AS data_consulta,
-    horario,
-    tipo_consulta,
-    retorno,
-    observacao
-FROM agendamento;
-        ;
-    `
+            id,
+            nome,
+            sexo,
+            cpf,
+            rg,
+            DATE_FORMAT(nascimento, '%d/%m/%Y') AS nascimento,
+            telefone,
+            email,
+            rua,
+            numero,
+            bairro,
+            cidade,
+            estado,
+            cep,
+            DATE_FORMAT(data_consulta, '%d-%m-%Y') AS data_consulta,
+            horario,
+            tipo_consulta,
+            retorno,
+            observacao
+        FROM agendamento
+    `;
 
+    if (id) {
+        q += ` WHERE id = ?`;
+
+        db.query(q, [id], (err, data) => {
+            if (err) {
+                return res.status(500).json({ message: 'Erro interno no servidor' });
+            }
+
+            if (data.length === 0) {
+                return res.status(404).json({ message: 'Agendamento não encontrado' });
+            }
+
+            return res.status(200).json(data[0]);
+        });
+
+    } else {
         db.query(q, (err, data) => {
             if (err) {
-                res.status(500).json('Erro interno no servidor')
+                return res.status(500).json({ message: 'Erro interno no servidor' });
             }
-            res.status(200).json(data)
-        })
-    } catch (err) {
-        console.error('Erro interno no servidor')
+
+            return res.status(200).json(data);
+        });
     }
-}
+};
+
+
 
 
 export const agendarConsulta = (req, res) => {
