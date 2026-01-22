7b54772b2ff7a9fa4d0ee625301c0f3c61aea231
Patrick <patrickhoffmanncampos@gmail.com>
2026-01-22 00:34:28 -0300

    feat(listagem agendamento): Listagem de atendimento com filtros finalizada

--- front/src/pages/Consultas/Consultas.jsx
+++ front/src/pages/Consultas/Consultas.jsx
@@ -1,6 +1,7 @@
 import { useLocation, Link } from 'react-router-dom'
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import { formatarData } from '../../utils/formatters'
 import "./style.css";
 
@@ -9,21 +10,43 @@ export default function Consultas() {
     const [consultas, setConsultas] = useState([])
     const [selectedConsulta, setSelectedConsulta] = useState(null);
 
+    const [nome, setNome] = useState("")
+    const [data, setData] = useState("")
+    const [proximas, setProximas] = useState(false)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(0)
+
     const location = useLocation();
     const [mensagem, setMensagem] = useState("");
 
     const getConsultas = async () => {
         try {
-            const res = await axios.get('http://localhost:8081/agendamento')
-            setConsultas(res.data)
+            const res = await axios.get(
+                `http://localhost:8081/agendamento?` +
+                `orderDir=ASC` +
+                `&nome=${nome}` +
+                `&data=${data}` +
+                `&proximos=${proximas}` +
+                `&limit=${limit}` +
+                `&page=${page}`
+            )
+            setConsultas(res.data.data)
+            setTotal(res.data.total)
         } catch (error) {
             console.error("Erro ao buscar consultas: ", error)
         }
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setPage(1)
+        getConsultas();
+    }
+
     useEffect(() => {
         getConsultas()
-    }, [])
+    }, [page, limit])
 
 
     useEffect(() => {
@@ -42,11 +65,20 @@ export default function Consultas() {
 
 
     const limitarTexto = (texto, limite = 20) => {
-        if(!texto) return "";
+        if (!texto) return "";
         return texto.length > limite
-        ? texto.slice(0, limite) + "..."
-        : texto
+            ? texto.slice(0, limite) + "..."
+            : texto
     }
+
+    function nextPage() {
+        setPage(prev => prev + 1)
+    }
+
+    function prevPage() {
+        if (page > 1) setPage(prev => prev - 1)
+    }
+
 
     return (
         <div className="consultas-container">
@@ -57,44 +89,99 @@ export default function Consultas() {
             )}
             <h1 className="consultas-title">Consultas Agendadas</h1>
 
+            {/* FILTROS */}
+            <div className="consultas-filtros">
+                <div className="filtro-grupo">
+                    <label>Pesquisar</label>
+                    <input
+                        type="text"
+                        placeholder="Nome do paciente"
+                        onChange={(e) => setNome(e.target.value)}
+                        value={nome}
+                    />
+                </div>
+
+                <div className="filtro-grupo">
+                    <label>Data da consulta</label>
+                    <input
+                        type="date"
+                        onChange={(e) => setData(e.target.value)}
+                        value={data}
+                    />
+                </div>
+
+                <div className="filtro-grupo checkbox">
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={proximas}
+                            onChange={(e) => setProximas(e.target.checked)}
+                        />
+                        Próximas consultas do dia
+                    </label>
+                </div>
+
+                <div className="filtro-grupo">
+                    <label>Registros por página</label>
+                    <select
+                        value={limit}
+                        onChange={(e) => setLimit(Number(e.target.value))}>
+                        <option value={5}>5</option>
+                        <option value={10}>10</option>
+                        <option value={20}>20</option>
+                        <option value={50}>50</option>
+                    </select>
+                </div>
+
+                <button className="btn-filtrar" onClick={handleSubmit}>
+                    Aplicar filtros
+                </button>
+            </div>
+
             {/* GRID DE CONSULTAS */}
             <div className="consultas-grid">
-                {consultas.map((consulta) => (
-                    <div
-                        key={consulta.id}
-                        className="consultas-card"
-                        onDoubleClick={() => setSelectedConsulta(consulta)}
-                    >
-                        <div className="consultas-card-header">
-                            <h3>{limitarTexto(consulta.nome)}</h3>
-                            <span className="consultas-tag">{consulta.tipo_consulta}</span>
-                        </div>
+                {consultas.length > 0 ? (
+                    consultas.map((consulta) => (
+                        <div
+                            key={consulta.id}
+                            className="consultas-card"
+                            onDoubleClick={() => setSelectedConsulta(consulta)}
+                        >
+                            <div className="consultas-card-header">
+                                <h3>{limitarTexto(consulta.nome)}</h3>
+                                <span className="consultas-tag">
+                                    {consulta.tipo_consulta}
+                                </span>
+                            </div>
 
-                        <div className="consultas-card-info-block">
-                            <p><strong>📅</strong> {consulta.data_consulta}</p>
-                            <p><strong>⏰</strong> {consulta.horario}</p>
-                            <p><strong>📍</strong> {consulta.cidade} - {consulta.estado}</p>
-                            <p><strong>☎️</strong> {consulta.telefone}</p>
-                        </div>
+                            <div className="consultas-card-info-block">
+                                <p><strong>📅</strong> {consulta.data_consulta}</p>
+                                <p><strong>⏰</strong> {consulta.horario}</p>
+                                <p><strong>📍</strong> {consulta.cidade} - {consulta.estado}</p>
+                                <p><strong>☎️</strong> {consulta.telefone}</p>
+                            </div>
 
-                        <div className="consultas-card-actions">
-                            <Link to={`http://localhost:5173/agenda/${consulta.id}`}>
+                            <div className="consultas-card-actions">
+                                <Link to={`/agenda/${consulta.id}`}>
+                                    <button className="consultas-btn editar">
+                                        Editar
+                                    </button>
+                                </Link>
+
                                 <button
-                                    className="consultas-btn editar"
+                                    className="consultas-btn detalhes"
+                                    onClick={() => setSelectedConsulta(consulta)}
                                 >
-                                    Editar
+                                    Ver detalhes
                                 </button>
-                            </Link>
-                            <button
-                                className="consultas-btn detalhes"
-                                onClick={() => setSelectedConsulta(consulta)}
-                            >
-                                Ver detalhes
-                            </button>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+                ) : (
+                    <div colSpan="4" style={{ textAlign: 'center' }}>Nenhuma consulta encontrada</div>
+                )}
             </div>
+
 
             {/* MODAL */}
             {selectedConsulta && (
@@ -144,6 +231,17 @@ export default function Consultas() {
                     </div>
                 </div>
             )}
+            <div className="pagination">
+                <FaArrowLeft
+                    className={`arrow ${page <= 1 ? 'disabled' : ''}`}
+                    onClick={page <= 1 ? null : prevPage}
+                />
+                <span>Página {page} de {Math.ceil(total / limit)}</span>
+                <FaArrowRight
+                    className={`arrow ${page >= Math.ceil(total / limit) ? 'disabled' : ''}`}
+                    onClick={page >= Math.ceil(total / limit) ? null : nextPage}
+                />
+            </div>
         </div>
     );
 }
