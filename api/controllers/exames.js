f0e189164006e2338ec1f0a187a67882d041820b
Patrick <patrickhoffmanncampos@gmail.com>
2026-07-24 09:12:59 -0300

    refactor: apresentando data completa da alteração no log

--- api/controllers/exames.js
+++ api/controllers/exames.js
@@ -354,9 +354,15 @@ export const logExames = (req, res) => {
 
     function formatarData(data) {
         const d = new Date(data);
+
         const dia = String(d.getDate()).padStart(2, '0');
         const mes = String(d.getMonth() + 1).padStart(2, '0');
         const ano = d.getFullYear();
-        return `${dia}/${mes}/${ano}`;
+
+        const horas = String(d.getHours()).padStart(2, '0');
+        const minutos = String(d.getMinutes()).padStart(2, '0');
+        const segundos = String(d.getSeconds()).padStart(2, '0');
+
+        return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
     }
 };
